8b809f5a8e460d5ed079b543651b29286d38646f
FreeFly19 <wmsystem2008@yandex.ru>
2016-10-31 18:42:03 +0200

    First changes

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("1231321");
+        System.out.println("123");
     }
 }
